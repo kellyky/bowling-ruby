5887970c9facd6cfdda4324a3a7e3ea7640d409d
Kelly Popko <kelpopko@gmail.com>
2024-08-13 00:57:41 -0500

    (refactor) Reorganize methods in Frame

--- frame.rb
+++ frame.rb
@@ -4,11 +4,10 @@ require_relative 'score'
 
 # Responsible for building each frame
 class Frame
-
   attr_reader :frames, :frame_number
   attr_accessor :rolls
 
-  # private
+  # Initialization and setup
   def initialize
     @frame_number = 1
     @frames = {
@@ -31,14 +30,44 @@ class Frame
     frames[frame_number]
   end
 
+  # Frame Management
   def build(pins)
     validate(pins)
-
     frames[frame_number] << pins
-
     frame_full? && !tenth_frame? and increment_frame_number
   end
 
+  def increment_frame_number
+    @frame_number += 1
+  end
+
+  def frame_full?
+    tenth_frame? ? frame_ten_full? : normal_frame_full?
+  end
+
+  def normal_frame_full?
+    rolls.first == Game::STRIKE || rolls.size == Game::MAX_ROLLS_PER_REGULAR_FRAME
+  end
+
+  def frame_ten_full?
+    rolls.size == 3 || (rolls.size == 2 && !qualifies_for_bonus_roll?)
+  end
+
+  def tenth_frame?
+    frame_number == 10
+  end
+
+  def normal_frame?
+    !tenth_frame?
+  end
+
+  def qualifies_for_bonus_roll?
+    tenth_frame? && rolls.size == 2 &&
+      strike?(rolls.first) || strike?(rolls.last) ||
+        spare?(rolls.first(2))
+  end
+
+  # Validation
   def validate(pins)
     Roll.validate(pins)
 
@@ -47,7 +76,7 @@ class Frame
 
   def too_many_pins?(roll)
     if normal_frame?
-      too_many_pins_normal_frame?(roll)
+      frames[frame_number].sum + roll > Game::PINS_PER_FRAME
     else
       too_many_pins_tenth_frame?(roll)
     end
@@ -60,46 +89,12 @@ class Frame
       strike?(rolls.first) && rolls[1] + roll > Game::PINS_PER_FRAME
   end
 
-  # TODO: check this one - aptly named? redundant?
-  def too_many_pins_normal_frame?(roll)
-    frames[frame_number].sum + roll > Game::PINS_PER_FRAME
-  end
-
-  def frame_full?
-    tenth_frame? ? frame_ten_full? : earlier_frame_full?
-  end
-
-  def tenth_frame?
-    frame_number == 10
-  end
-
-  def normal_frame?
-    !tenth_frame?
-  end
-
-  def increment_frame_number
-    @frame_number += 1
-  end
-
-  def qualifies_for_bonus_roll?
-    tenth_frame? && rolls.size == 2 &&
-      strike?(rolls.first) || strike?(rolls.last) ||
-        spare?(rolls.first(2))
-  end
-
-  def frame_ten_full?
-    rolls.size == 3 || (rolls.size == 2 && !qualifies_for_bonus_roll?)
-  end
-
+  # Helper Methods
   def strike?(roll)
     roll == Game::STRIKE
   end
 
   def spare?(rolls)
     rolls.sum == Game::PINS_PER_FRAME
-  end
-
-  def earlier_frame_full?
-    rolls.first == Game::STRIKE || rolls.size == Game::MAX_ROLLS_PER_REGULAR_FRAME
   end
 end
